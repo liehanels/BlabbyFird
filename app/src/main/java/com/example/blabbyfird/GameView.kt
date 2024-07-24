b721a89793913b3dddf4916f19354464e04c7de7
Liehan Els <ST10085345@vcconnect.edu.za>
2024-07-24 11:15:23 +0200

    Blabby Fird first commit

---
+++ app/src/main/java/com/example/blabbyfird/GameView.kt
@@ -0,0 +1,47 @@
+package com.example.blabbyfird
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class GameView(context: Context) : SurfaceView(context), SurfaceHolder.Callback {
+    private val thread: GameThread
+    private val birb: Birb
+    private val towers = mutableListOf<Tower>()
+    private val paint = Paint()
+    private val background: Bitmap
+    private var score = 0
+    private var startTime: Long = 0
+
+    init {
+        holder.addCallback(this)
+        birb = Birb(context)
+        towers.add(Tower(context, 800f, 400))
+        towers.add(Tower(context, 1600f, 300))
+        thread = GameThread(holder, this)
+        background = BitmapFactory.decodeResource(resources, R.drawable.background)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        val paint = Paint()
+        paint.color = Color.BLUE
+        paint.strokeWidth = 7f
+        paint.style = Paint.Style.FILL
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        TODO("Not yet implemented")
+    }
+
+    fun update() {
+        TODO("Not yet implemented")
+    }
+}
