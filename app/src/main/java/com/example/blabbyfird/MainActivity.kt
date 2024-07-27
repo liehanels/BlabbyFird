08cf691adb1bde3d318645be7f0289d7641827d9
Liehan Els <ST10085345@vcconnect.edu.za>
2024-07-27 12:18:40 +0200

    fixed buttonStart but now it keeps closing

--- app/src/main/java/com/example/blabbyfird/MainActivity.kt
+++ app/src/main/java/com/example/blabbyfird/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.blabbyfird
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.blabbyfird.Model.ScreenSize
 import com.example.blabbyfird.UI.PlayActivity
 
 class MainActivity : AppCompatActivity() {
     private val Tag = "MainActivity"
+    private lateinit var buttonStart: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ScreenSize.getScreenSize(this)
-        buttonStart.SetOnClickListener {
+        buttonStart = findViewById(R.id.buttonStart)
+
+        buttonStart.setOnClickListener() {
             val iPlayGame = Intent(this@MainActivity, PlayActivity::class.java)
             startActivity(iPlayGame)
             finish()
